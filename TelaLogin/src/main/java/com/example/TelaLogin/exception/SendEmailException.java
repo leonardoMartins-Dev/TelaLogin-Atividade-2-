30c548272b98e1c8a15666d8541e530c422c1e0b
leo martins <leo.euricobete@email.com>
2026-09-17 17:32:41 -0300

    recovery password

---
+++ TelaLogin/src/main/java/com/example/TelaLogin/exception/SendEmailException.java
@@ -0,0 +1,8 @@
+package com.example.TelaLogin.exception;
+
+
+public class SendEmailException extends RuntimeException {
+    public SendEmailException(String message) {
+        super(message);
+    }
+}
